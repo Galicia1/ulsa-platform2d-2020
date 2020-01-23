201ae83d65d0d0a7427214a3564a38501530452f
Jorge Galicia <jorgeluis.gv@hotmail.com>
2020-01-23 07:45:04 -0700

    jump

---
+++ Assets/Scripts/Character2D.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Platform2DUtils.GameplaySystem;
+
+public class Character2D : MonoBehaviour
+{
+    protected SpriteRenderer spr;
+    protected Animator anim;
+   
+    [SerializeField]
+    protected float moveSpeed = 2f;
+
+    void Awake()
+    {
+        spr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    protected bool FlipSprite
+    {
+        get => GameplaySystem.Axis.x < 0 ? true : GameplaySystem.Axis.x > 0 ? false : spr.flipX;
+    }
+
+}
