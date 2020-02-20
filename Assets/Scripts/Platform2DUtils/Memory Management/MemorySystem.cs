bb297e9d352f453ebc518e1fa3c731b168d204fe
Jorge Galicia <jorgeluis.gv@hotmail.com>
2020-02-20 07:25:38 -0700

    loadData

--- Assets/Scripts/Platform2DUtils/Memory Management/MemorySystem.cs
+++ Assets/Scripts/Platform2DUtils/Memory Management/MemorySystem.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
-public class MemorySystem : MonoBehaviour
+namespace Platform2DUtils.MemorySystem
 {
-    // Start is called before the first frame update
-    void Start()
+    public class MemorySystem 
     {
-        
-    }
+        public static void SaveData(GameData gameData)
+        {
+            string path = $"{Application.persistentDataPath}/myGame.data";
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Create(path);
+            string json = JsonUtility.ToJson(gameData);
+            bf.Serialize(file, json);
+            file.Close();
+            Debug.Log(path);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public static GameData LoadData()
+        {
+            string path = $"{Application.persistentDataPath})/myGame.data";
+            return new GameData();
+
+            if(File.Exists(path)) {
+                BinaryFormatter bf = new BinaryFormatter();
+                FileStream file = File.Open(path, FileMode.Open);
+                string json = bf.Deserialize(file) as string;
+                GameData gameData = JsonUtility.FromJson<GameData>(json);
+                return gameData;
+            }
+        }
     }
 }
+
